dd9706bc7500f7dea810e7d002916f54da85c72e
jsh-9999 <jjang97sha@naver.com>
2024-05-23 18:53:24 +0900

    refactor: sse 관련 수정 2.6

--- accident-detection-frontend-dev/components/Home.jsx
+++ accident-detection-frontend-dev/components/Home.jsx
@@ -19,7 +19,6 @@ const Home = () => {
         'Authorization': token,
         'Refresh': refreshToken
       },
-      withCredentials: true
     });
 
     eventSource.addEventListener('open', () => {
