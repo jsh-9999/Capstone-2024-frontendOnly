6702f2fe31affd8be1b0685335f05d927b104792
jsh-9999 <jjang97sha@naver.com>
2024-05-24 13:59:25 +0900

    refactor: 토큰 인증 방식 수정

--- accident-detection-frontend-dev/components/charts/SimpleChart.tsx
+++ accident-detection-frontend-dev/components/charts/SimpleChart.tsx
@@ -12,7 +12,6 @@ import {
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
 import { useQuery } from "@tanstack/react-query";
-import { getCookie } from 'cookies-next';
 
 ChartJS.register(
   CategoryScale,
@@ -55,8 +54,8 @@ type Props = {};
 
 export function SimpleChart({}: Props) {
   const fetchMonthlyAccidentData = async (): Promise<Record<string, number>> => {
-    const token = getCookie("Authorization");
-    const refreshToken = getCookie("Refresh");
+    const token = localStorage.getItem("Authorization");
+    const refreshToken = localStorage.getItem("Refresh");
 
     if (!token || !refreshToken) {
       throw new Error("No token found");
@@ -64,7 +63,7 @@ export function SimpleChart({}: Props) {
 
     const response = await fetch("https://backend-capstone.site/api/hospital/accident/statistics/month", {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: token,
         Refresh: `${refreshToken}`
       },
       credentials: 'include',
